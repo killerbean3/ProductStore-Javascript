bf6e0c45080d453269e6e93bf5128c5fe88937ec
Dom <15095331+killerbean3@users.noreply.github.com>
2024-04-16 13:11:01 +0100

    changes

--- src/utils.js
+++ src/utils.js
@@ -3,12 +3,11 @@
 //   DATA IS THE SAME JUST A DIFFERENT URL,
 //   DOES NOT AFFECT PROJECT FUNCTIONALITY
 
-const allProductsUrl =
-  "https://cors-anywhere.herokuapp.com/course-api.com/javascript-store-products";
+const allProductsUrl = "https://course-api.com/javascript-store-products";
 // temporary single product
 // 'https://course-api.com/javascript-store-single-product?id=rec43w3ipXvP28vog'
 const singleProductUrl =
-  "https://cors-anywhere.herokuapp.com/course-api.com/javascript-store-single-product";
+  "https://course-api.com/javascript-store-single-product";
 
 const getElement = (selection) => {
   const element = document.querySelector(selection);
