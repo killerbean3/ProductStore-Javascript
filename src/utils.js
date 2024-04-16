5e581a4cafefb127a32ce93ee14bb3dab808eaf0
Dom <15095331+killerbean3@users.noreply.github.com>
2024-04-16 12:58:20 +0100

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -3,48 +3,42 @@
 //   DATA IS THE SAME JUST A DIFFERENT URL,
 //   DOES NOT AFFECT PROJECT FUNCTIONALITY
 
-<<<<<<< HEAD
 const allProductsUrl =
   "https://cors-anywhere.herokuapp.com/course-api.com/javascript-store-products";
 // temporary single product
 // 'https://course-api.com/javascript-store-single-product?id=rec43w3ipXvP28vog'
 const singleProductUrl =
   "https://cors-anywhere.herokuapp.com/course-api.com/javascript-store-single-product";
-=======
-const allProductsUrl = 'https://course-api.com/javascript-store-products'
-// temporary single product
-// 'https://course-api.com/javascript-store-single-product?id=rec43w3ipXvP28vog'
-const singleProductUrl =
-  'https://course-api.com/javascript-store-single-product'
->>>>>>> f5f6743e3627e657b6029af8f6ef44f4e876489a
 
 const getElement = (selection) => {
-  const element = document.querySelector(selection)
-  if (element) return element
-  throw new Error(`Please check "${selection}" selector, no such element exist`)
-}
+  const element = document.querySelector(selection);
+  if (element) return element;
+  throw new Error(
+    `Please check "${selection}" selector, no such element exist`
+  );
+};
 
 const formatPrice = (price) => {
-  let formattedPrice = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-  }).format((price / 100).toFixed(2))
-  return formattedPrice
-}
+  let formattedPrice = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  }).format((price / 100).toFixed(2));
+  return formattedPrice;
+};
 
 const getStorageItem = (item) => {
-  let storageItem = localStorage.getItem(item)
+  let storageItem = localStorage.getItem(item);
   if (storageItem) {
-    storageItem = JSON.parse(localStorage.getItem(item))
+    storageItem = JSON.parse(localStorage.getItem(item));
   } else {
-    storageItem = []
+    storageItem = [];
   }
-  return storageItem
-}
+  return storageItem;
+};
 
 const setStorageItem = (name, item) => {
-  localStorage.setItem(name, JSON.stringify(item))
-}
+  localStorage.setItem(name, JSON.stringify(item));
+};
 
 export {
   allProductsUrl,
@@ -53,4 +47,4 @@ export {
   formatPrice,
   getStorageItem,
   setStorageItem,
-}
+};
